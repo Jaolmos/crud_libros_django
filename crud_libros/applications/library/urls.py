6059627f9b8eda7090a6dc15e94c59e6f0b61f62
josea <jose.a.olmosmartinez@gmail.com>
2022-09-18 19:38:55 +0200

    configumos para poder ver las imagenes

--- crud_libros/applications/library/urls.py
+++ crud_libros/applications/library/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .import views 
+from .import views
+from django.conf import settings
+from django.contrib.staticfiles.urls import static 
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -8,5 +10,5 @@ urlpatterns = [
     path('create-book', views.createBook, name='create_book'),
     path('edit-book', views.editBook, name='edit_book')
     
-]
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
