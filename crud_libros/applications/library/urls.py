4b7d16aa6e1861ab1af3a48d0b1b62cfe44e38b9
josea <jose.a.olmosmartinez@gmail.com>
2022-09-18 14:13:16 +0200

    Creacion de inicio, base y enlace

--- crud_libros/applications/library/urls.py
+++ crud_libros/applications/library/urls.py
@@ -3,5 +3,7 @@ from .import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about-us', views.aboutUs, name='about_us')
+    path('about-us', views.aboutUs, name='about_us'),
+    path('books', views.books, name='books')
+
 ]
--- crud_libros/applications/library/views.py
+++ crud_libros/applications/library/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Eres un gran programador Jose! yo confio en ti! llegaras lejos!! </h1>")
+    return render(request, 'pages/index.html')
 
 def aboutUs(request):  #Función que nos retorna la pagina html de acerca de nosotros.
     return render(request, 'pages/about_us.html')
+
+def books(request):
+    return render(request, 'books/books.html')
