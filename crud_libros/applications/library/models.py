c773e8f04fa291a92a234846ec44b6ada60a5824
josea <jose.a.olmosmartinez@gmail.com>
2022-09-18 18:35:03 +0200

    se configura administrador

--- crud_libros/applications/library/models.py
+++ crud_libros/applications/library/models.py
@@ -1,3 +1,8 @@
+from distutils.command.upload import upload
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(verbose_name='Titulo', max_length=100)
+    image = models.ImageField(verbose_name='Imagenes',upload_to='images/', null=True)
+    description = models.TextField(verbose_name='Descripción', max_length=300)
